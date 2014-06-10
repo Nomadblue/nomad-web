511e88821a6ea15581a25c98bc1f69b1a8fa7161
Hector Garcia <hector@nomadblue.com>
2014-06-10 10:02:58 -0400

    Fix post edit in admin

--- blog/admin.py
+++ blog/admin.py
@@ -4,10 +4,10 @@ from blog.models import NomadPost
 
 
 class NomadPostAdmin(admin.ModelAdmin):
-    # list_display = ('title', 'status', 'category', 'summary')
     list_display = ('title', 'status', 'pub_date', 'slug', 'summary')
     list_editable = ('pub_date',)
     search_fields = ('title', 'summary')
-    fields = ('bloguser', 'status', 'title', 'slug', 'summary', 'content')
+    fields = ('bloguser', 'pub_date', 'status', 'title', 'slug', 'summary', 'content', 'categories')
+    prepopulated_fields = {'slug': ('title',)}
 
 admin.site.register(NomadPost, NomadPostAdmin)
