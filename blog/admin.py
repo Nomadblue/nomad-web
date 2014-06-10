f2c459107bfc3e3875476f68c639796a51ef345a
Hector Garcia <hector@nomadblue.com>
2014-06-10 12:13:52 -0400

    Add post image

--- blog/admin.py
+++ blog/admin.py
@@ -7,7 +7,7 @@ class NomadPostAdmin(admin.ModelAdmin):
     list_display = ('title', 'status', 'pub_date', 'slug', 'summary')
     list_editable = ('pub_date',)
     search_fields = ('title', 'summary')
-    fields = ('bloguser', 'pub_date', 'status', 'title', 'slug', 'summary', 'content', 'categories')
+    exclude = ('featured_countries',)
     prepopulated_fields = {'slug': ('title',)}
 
 admin.site.register(NomadPost, NomadPostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 
+from imagekit import ImageSpec, register
+from imagekit.models.fields import ImageSpecField
+from imagekit.processors import ResizeToFill, ResizeToFit, Adjust
+
 from nomadblog.models import Post
 
 
 class NomadPost(Post):
     summary = models.TextField()
+    image = models.ImageField(upload_to="images/posts/%Y/%m/%d", max_length=500, blank=True)
 
     def __unicode__(self):
         return self.title
@@ -15,12 +20,13 @@ class NomadPost(Post):
         category = self.categories.latest('id')
         return reverse('post_detail', kwargs={'category_slug': category.slug, 'slug': self.slug})
 
+    # Imagekit specs
+    pic = ImageSpecField([Adjust(contrast=1.2, sharpness=1.1), ResizeToFit(720)], source='image', format='PNG', options={'quality': 90})
+
 
 # This code here to be used alogn with the {% generateimage %} imagekit template tag
 # http://django-imagekit.readthedocs.org/en/latest/#generateimage
 # https://github.com/matthewwithanm/django-imagekit/issues/202
-from imagekit import ImageSpec, register
-from imagekit.processors import ResizeToFill
 
 
 class BlogUserAvatarThumbnail(ImageSpec):
