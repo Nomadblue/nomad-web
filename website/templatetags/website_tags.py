9ffef4eb132d20d47bcb2cb5d39f58ff8ab86216
Hector Garcia <hector@nomadblue.com>
2014-06-09 20:52:04 -0400

    Make navbar activate current menu item

---
+++ website/templatetags/website_tags.py
@@ -0,0 +1,18 @@
+import re
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag
+def active_nav_menu_item(request, pattern):
+    if re.search("^%s$" % pattern, request.path):
+        return 'active'
+    return ''
+
+
+@register.simple_tag
+def active_nav_menu_item_contains(request, pattern):
+    if re.search("^%s" % pattern, request.path):
+        return 'active'
+    return ''
