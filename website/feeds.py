5162b08c52b61b21d1cd6db8efaab740c53b1337
Hector Garcia <hector@nomadblue.com>
2014-06-26 17:22:40 -0400

    Fix feeds translations

--- website/feeds.py
+++ website/feeds.py
@@ -1,5 +1,6 @@
 from django.contrib.syndication.views import Feed
 from django.db.models import Q
+from django.utils.translation import ugettext_lazy as _
 
 from nomadblog import get_post_model
 
@@ -12,12 +13,12 @@ POST_MODEL = get_post_model()
 
 
 class LatestEntries(Feed):
-    title = "Nomadblue.com"
+    title = _("Nomadblue - We are your CTO on-demand")
     link = "/"
-    description = "Nomadblue.com: Latest entries"
+    description = _("Nomadblue: Latest entries")
 
     def items(self):
-        return POST_MODEL.objects.order_by('-pub_date')[:10]
+        return POST_MODEL.objects.filter(status=POST_MODEL.PUBLIC_STATUS).order_by('-pub_date')[:10]
 
     def item_title(self, item):
         return item.title
@@ -27,12 +28,12 @@ class LatestEntries(Feed):
 
 
 class LatestEntriesDjango(Feed):
-    title = "Nomadblue.com"
+    title = _("Nomadblue - We are your CTO on-demand")
     link = "/blog/django/"
-    description = "Nomadblue.com: Latest entries in Django category"
+    description = _("Nomadblue: Latest entries in Django category")
 
     def items(self):
-        return POST_MODEL.objects.filter(categories__name__iexact='django').order_by('-pub_date')[:10]
+        return POST_MODEL.objects.filter(status=POST_MODEL.PUBLIC_STATUS).filter(categories__name__iexact='django').order_by('-pub_date')[:10]
 
     def item_title(self, item):
         return item.title
@@ -42,12 +43,12 @@ class LatestEntriesDjango(Feed):
 
 
 class LatestEntriesPython(Feed):
-    title = "Nomadblue.com"
+    title = _("Nomadblue - We are your CTO on-demand")
     link = "/blog/python/"
-    description = "Nomadblue.com: Latest entries in Python category"
+    description = _("Nomadblue: Latest entries in Python category")
 
     def items(self):
-        return POST_MODEL.objects.filter(Q(categories__name__iexact='django') | Q(categories__name__iexact='python')).order_by('-pub_date')[:10]
+        return POST_MODEL.objects.filter(status=POST_MODEL.PUBLIC_STATUS).filter(Q(categories__name__iexact='django') | Q(categories__name__iexact='python')).order_by('-pub_date')[:10]
 
     def item_title(self, item):
         return item.title
